0ef3a7da026e20ee3c374c9f9657ff864d3dfc53
justinjmueller <justin.mueller@colostate.edu>
2025-02-15 18:00:05 -0600

    Fix forgotten reference to bare pid attribute

--- cafana/include/particle_variables.h
+++ cafana/include/particle_variables.h
@@ -50,7 +50,7 @@ namespace pvars
             }
             else
             {
-                if(p.pid < 2) energy += p.calo_ke;
+                if(PIDFUNC(p) < 2) energy += p.calo_ke;
                 else
                 {
                     if(p.is_contained) energy += p.csda_ke;
